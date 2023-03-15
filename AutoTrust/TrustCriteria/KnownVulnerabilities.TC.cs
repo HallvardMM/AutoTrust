78dd6ba8bf6d6cacea0fb5d6bb3d0fb090eb7162
Sverre Rynning-Tønnesen <sverrert98@gmail.com>
2023-03-15 15:19:12 +0100

    removed unused functions in knownVulnerabilities

--- AutoTrust/TrustCriteria/KnownVulnerabilities.TC.cs
+++ AutoTrust/TrustCriteria/KnownVulnerabilities.TC.cs
@@ -6,15 +6,6 @@ public class KnownVulnerabilities : ITrustCriteria {
   public static Status Validate(DataHandler dataHandler) {
     // Check if the package has any known vulnerabilities
 
-    // if (dataHandler.NugetCatalogEntry.Vulnerabilities != null) {
-    //   Console.WriteLine("NuGet has found known vulnerabilities");
-    //   Console.WriteLine("NUGET: " + dataHandler.NugetCatalogEntry.Vulnerabilities[0]);
-    // }
-    // if (dataHandler.OsvData.Vulns != null) {
-    //   Console.WriteLine("Osv has found known vulnerabilities");
-    //   Console.WriteLine("OSV: " + dataHandler.OsvData.Vulns[0]);
-    // }
-
     if (dataHandler.NugetCatalogEntry?.Vulnerabilities != null || dataHandler.OsvData?.Vulns != null) {
       PrettyPrint.FailPrint("Package has known vulnerabilities");
       return Status.Error;

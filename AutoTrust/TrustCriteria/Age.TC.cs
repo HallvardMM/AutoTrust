a1df8d7a5660790e5c5ad28e43c254950d34ee4f
HallvardMM <hallvardmolinmorstol@gmail.com>
2023-03-16 12:51:44 +0100

    Added check if package version is older than 365 days

--- AutoTrust/TrustCriteria/Age.TC.cs
+++ AutoTrust/TrustCriteria/Age.TC.cs
@@ -4,6 +4,7 @@ public class Age : ITrustCriteria {
   public string Title => "Package age";
 
   private static readonly int VersionAgeInDaysThreshold = 22; //Same as npq
+  private static readonly int VersionOldAgeInDaysThreshold = 365;
   public static Status Validate(DataHandler dataHandler) {
     // Check if the package version is older than a certain threshold
     // Check if the package version is a pre-release version
@@ -18,6 +19,13 @@ public class Age : ITrustCriteria {
 
     if (ageOfVersion < VersionAgeInDaysThreshold) {
       PrettyPrint.FailPrint($"Package version was created less than {VersionAgeInDaysThreshold} days ago: {ageOfVersion} days");
+      if (packageIsPreRelease) {
+        PrettyPrint.WarningPrint($"Package version is a pre-release version: {dataHandler.NugetCatalogEntry?.Version}");
+      }
+      return Status.Fail;
+    }
+    if (ageOfVersion > VersionOldAgeInDaysThreshold) {
+      PrettyPrint.FailPrint($"Package version was created more than {VersionOldAgeInDaysThreshold} days ago: {ageOfVersion} days");
       if (packageIsPreRelease) {
         PrettyPrint.WarningPrint($"Package version is a pre-release version: {dataHandler.NugetCatalogEntry?.Version}");
       }

1dd94b13ee046e6d58bce7dfd16e599a2ece0c1e
HallvardMM <hallvardmolinmorstol@gmail.com>
2023-03-28 18:05:09 +0200

    logic for new and old vulnerabilities. Fail=Current, Warning=Old, Pass=none

--- AutoTrust/TrustCriteria/KnownVulnerabilities.TC.cs
+++ AutoTrust/TrustCriteria/KnownVulnerabilities.TC.cs
@@ -4,14 +4,35 @@ public class KnownVulnerabilities : ITrustCriteria {
   public string Title => "Known Vulnerabilities";
 
   public static Status Validate(DataHandler dataHandler) {
-    // Check if the package has any known vulnerabilities
+    long oldVulnerabilities = 0;
+    long currentVulnerabilities = 0;
 
-    if (dataHandler.NugetCatalogEntry?.Vulnerabilities != null || dataHandler.OsvData?.Vulns != null) {
-      PrettyPrint.FailPrint("Package has known vulnerabilities");
+    if (dataHandler.NugetCatalogEntry?.Vulnerabilities != null) {
+      PrettyPrint.FailPrint($"Package has {dataHandler.NugetCatalogEntry?.Vulnerabilities.Count} known vulnerabilities registered in NuGet");
       return Status.Fail;
     }
 
-    PrettyPrint.SuccessPrint("No current vulnerabilities found");
+    //Check all Osv vulnerabilities
+    dataHandler.OsvData?.Vulns?.ForEach(vulnerability => {
+      vulnerability.Affected.ForEach(affectedPackage => {
+        if (affectedPackage.Versions.Contains(dataHandler.PackageVersion)) {
+          currentVulnerabilities++;
+        }
+      });
+      oldVulnerabilities++;
+    });
+
+    if (currentVulnerabilities > 0) {
+      PrettyPrint.FailPrint($"Package has {currentVulnerabilities} current vulnerabilities registered in OSV");
+      return Status.Fail;
+    }
+
+    if (oldVulnerabilities > 0) {
+      PrettyPrint.WarningPrint($"Package has {oldVulnerabilities} known vulnerabilities registered in OSV but not for the current version");
+      return Status.Error;
+    }
+
+    PrettyPrint.SuccessPrint("No current or old vulnerabilities found");
     return Status.Pass;
   }
 }
